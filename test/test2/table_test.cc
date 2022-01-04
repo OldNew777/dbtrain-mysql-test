371bf8822fd49b128cc0b64f11daea017f28ccb7
ChenXin <chenxin18@mails.tsinghua.edu.cn>
2022-01-04 15:34:08 +0800

    add range test

--- test/test2/table_test.cc
+++ test/test2/table_test.cc
@@ -4,17 +4,22 @@
 
 namespace dbtrain_mysql {
 
-TEST(DatabaseTest, SaveTest) {
+TEST(TableTest, RangeTest) {
   Clear();
   Init();
   Instance* instance = new Instance();
   Execute(instance, "CREATE DATABASE THU;");
   Execute(instance, "USE THU;");
   Execute(instance, "CREATE TABLE student(name VARCHAR(2000), id INT);");
-  delete instance;
-  instance = new Instance();
-  Execute(instance, "USE THU;");
-  Execute(instance, "DESC student;");
+
+  for (int i = 0; i < 10; ++i)
+    Execute(instance, "INSERT INTO student VALUES('Jack',20);");
+  Execute(instance, "INSERT INTO student VALUES('Mike',NULL);");
+  Execute(instance, "INSERT INTO student VALUES('Mike', NULL);");
+  for (int i = 0; i < 10; ++i)
+    Execute(instance, "INSERT INTO student VALUES(NULL,872);");
+
+  Execute(instance, "SELECT * FROM student WHERE student.id > 30;");
   Execute(instance, "DROP DATABASE THU;");
   delete instance;
   Clear();
